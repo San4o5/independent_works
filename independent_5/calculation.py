c8cf90ca9492406b9e7c7307674772e136465f51
San4o5 <babaimal2006@gmail.com>
2025-04-26 15:58:37 +0300

    v 1.0

--- independent_5/calculation.py
+++ independent_5/calculation.py
@@ -1,10 +1,10 @@
 import math
 
 def sin_taylor(x, eps):
-    term = x 
+    term = x #перше доданок у ряді
     sum_ = x  
-    n = 1
-    iterations = 1
+    n = 1 #лічильник для формули факторіалів
+    iterations = 1 #кількість ітерацій
 
     while abs(term) >= eps:
         term *= -x * x / ((2 * n) * (2 * n + 1))
